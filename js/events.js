6990514aabfe57a29b705cd124faa2e8d70d3de2
viqi zevic <vicky.tanzil@gmail.com>
2018-09-05 19:35:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,12 @@
 //define functions here
-
-$(document).ready(function(){
-
-// call functions here
 function getIt() {
   $('p').on("click", function() {
     alert("Hey!");
   });
 }
+
+$(document).ready(function(){
+
+// call functions here
 
 });
